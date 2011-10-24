da8ff2bac4cc4b82a0fc211071e7f80d51d74391
John Barton (joho) <jrbarton@gmail.com>
2011-10-24 22:57:09 +1100

    add an ackrc so i can search within haml filesw

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 ln -sf $PWD/vimrc ~/.vimrc
 ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/profile ~/.bash_profile
-ln -sf $PWD/shell-scripts ~/scripts
+# ln -sf $PWD/shell-scripts ~/scripts
 ln -sf $PWD/rdbugrc ~/.rdbugrc
 ln -sf $PWD/irbrc ~/.irbrc
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/git-completion.sh ~/.git-completion.sh
 ln -sf $PWD/gemrc ~/.gemrc
+ln -sf $PWD/ackrc ~/.ackrc
