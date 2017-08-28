4022d4027074fbbfd93b567f903adcf2ce295c5a
John Barton (joho) <jrbarton@gmail.com>
2017-08-28 22:35:25 +1000

    Remove rdebugrc and add missing install steps

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@ ln -sf $PWD/gvimrc ~/.gvimrc
 ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/profile ~/.bash_profile
-# ln -sf $PWD/shell-scripts ~/scripts
-ln -sf $PWD/rdbugrc ~/.rdbugrc
-ln -sf $PWD/irbrc ~/.irbrc
 ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sf $PWD/gitignore_global ~/.gitignore_global
 ln -sf $PWD/gemrc ~/.gemrc
 ln -sf $PWD/ackrc ~/.ackrc
+ln -sf $PWD/psqlrc ~/.psqlrc
