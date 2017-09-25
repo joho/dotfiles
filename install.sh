9bfe7e5bf35c00960da073763b5a97438b3c6141
John Barton (joho) <jrbarton@gmail.com>
2017-09-25 19:12:05 +1000

    Start tracking vscode config

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@ if ! command -v brew >/dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+if [[ "$uname 2> /dev/null" == "Linx" ]]; then
+  ln -sf "$PWD/vscode/settings.json" ~/.config/Code/User/settings.json
+  ln -sf "$PWD/vscode/snippets" ~/.config/Code/User/snippets
+elif
+  ln -sf "$PWD/vscode/settings.json" "~/Library/Application Support/Code/User/settings.json"
+  ln -sf "$PWD/vscode/snippets" "~/Library/Application Support/Code/User/snippets"
+fi
+
 
 ln -sf "$PWD/vimrc" ~/.vimrc
 ln -sf "$PWD/gvimrc" ~/.gvimrc
